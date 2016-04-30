c55ac15a14ad71d41a0778f700ef1dbc823a39b2
jatinrev <jatin@revinfotech.com>
2016-04-30 12:42:52 +0530

    added the functionality of review page.

--- www/components/Review/review.js
+++ www/components/Review/review.js
@@ -7,14 +7,43 @@ angular.module('LoyalBonus')
 		.then(function (res) {
 			return res.data.Data;
 		});
-	}	
+	}
+
+	function giveFeedback(rating, pros, cons, businessId, userId) {
+		return ajaxCall
+		.post('webapi/BusinessMaster/BusinessGiveStar',
+			  {
+			  	BusinessId 	: businessId,
+				UserId		: userId,
+				Pros 		: pros,
+				Cons 		: cons
+			   }
+		)
+		.then(function(prosConsResult) {
+			return ajaxCall
+			.post('webapi/BusinessMaster/BusinessGiveHeartStar',
+				  {
+				  	UserId 		: userId, 
+				  	BusinessId  : businessId,
+				   	Reviews 	: rating
+				   }
+			)
+			.then(function(ratingResult) {
+				console.log(prosConsResult);
+				console.log(ratingResult);
+				return prosConsResult;
+			});
+			
+		});
+	}
 
 	return {
-		printReview : printReview
+		printReview  : printReview,
+		giveFeedback : giveFeedback
 	};
 })
 
-.controller('ReviewController', function ($scope, $state, showRating, reviewFactory, showRating) {
+.controller('ReviewController', function ($scope, $state, showRating, reviewFactory, showRating, $rootScope) {
 	var allReviews = [];
 	var reviewPage = 0;
 	$scope.reviewVar = {
@@ -59,9 +88,46 @@ angular.module('LoyalBonus')
     	return allReviews;
     }
 
+    /***Rating starts here***/
+
+    $scope.reviewVar.ratingStarNumber = 0;
+
+    $scope.reviewVar.giveRating = function(number) {
+    	$scope.reviewVar.ratingStarNumber = number;
+    	$scope.reviewVar.starsArr = [];
+		for( var i = 1; i < 6; i++ ) {
+			if( i > 0 && i <= number) {
+				$scope.reviewVar.starsArr.push({ 
+					class : 'filledStart',
+					src   : 'img/filledStar.png'
+				});
+			} else {
+				$scope.reviewVar.starsArr.push({ 
+					class : 'emptyStart',
+					src   : 'img/emptyStart.png'
+				});
+			}
+		}
+    }
+    $scope.reviewVar.giveRating(0);
 	// console.log(giveRating.ratingImages(2));
    
+	$scope.reviewVar.ratingMsg = false;
 
+	$scope.reviewVar.giveFeedback = function() {
+		reviewFactory
+		.giveFeedback($scope.reviewVar.ratingStarNumber
+					 ,$scope.reviewVar.pros
+					 ,$scope.reviewVar.cons
+					 ,$state.params.businessId
+					 ,$rootScope.userDetails.userId
+		)
+		.then(function(res) {
+			$scope.reviewVar.ratingMsg = true;
+			$scope.stopLoading = true;
+			$scope.reviewVar.loadMore();
+		});
+	}
 	
 });
 
