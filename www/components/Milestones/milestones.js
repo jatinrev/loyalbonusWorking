4ac682df2a0a70c3bf4a2b68181982d4cecc781e
Revinfotech <lalitdogra04@gmail.com>
2016-05-16 17:39:23 +0530

    point

--- www/components/Milestones/milestones.js
+++ www/components/Milestones/milestones.js
@@ -22,9 +22,10 @@ angular.module('LoyalBonus')
 	ajaxCall
 	.get('webapi/BusinessMaster/GetAllBusinessLocationsVisitedByUser?UserId='+$rootScope.userDetails.userId, {})
 	.then(function(res) {
-		console.log(res);
+		//console.log(res);
 		$scope.myloyalbonus.print = res.data.Data;
 		//console.log($scope.myloyalbonus.print);
+		
 	});
 
 	
