032239258a67eeee373fce12bfc3cbfd4bac5f87
jatinrev <jatin@revinfotech.com>
2016-05-16 13:29:30 +0530

    kuch b

--- www/components/Restaurants/restaurants.js
+++ www/components/Restaurants/restaurants.js
@@ -104,19 +104,7 @@ angular.module('LoyalBonus', '')
               //  alert("You have selected " + data.index + " tab");
             //};
 
-            $scope.goForward = function () {
-        var selected = $ionicTabsDelegate.selectedIndex();
-        if (selected != -1) {
-            $ionicTabsDelegate.select(selected + 1);
-        }
-    }
-
-    $scope.goBack = function () {
-        var selected = $ionicTabsDelegate.selectedIndex();
-        if (selected != -1 && selected != 0) {
-            $ionicTabsDelegate.select(selected - 1);
-        }
-    }
+           
             
 
 
