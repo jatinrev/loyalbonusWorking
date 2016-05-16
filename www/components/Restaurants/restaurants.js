14e22d531b6d41ad981f2d7152fbd9357fb4b843
Revinfotech <lalitdogra04@gmail.com>
2016-05-16 12:13:20 +0530

    tabs

--- www/components/Restaurants/restaurants.js
+++ www/components/Restaurants/restaurants.js
@@ -104,7 +104,19 @@ angular.module('LoyalBonus', '')
               //  alert("You have selected " + data.index + " tab");
             //};
 
-            
+            $scope.goForward = function () {
+        var selected = $ionicTabsDelegate.selectedIndex();
+        if (selected != -1) {
+            $ionicTabsDelegate.select(selected + 1);
+        }
+    }
+
+    $scope.goBack = function () {
+        var selected = $ionicTabsDelegate.selectedIndex();
+        if (selected != -1 && selected != 0) {
+            $ionicTabsDelegate.select(selected - 1);
+        }
+    }
             
 
 
