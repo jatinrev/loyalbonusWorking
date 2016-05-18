9d80f3f7b8d309b0f32d35e3b49a778940ec68be
Revinfotech <lalitdogra04@gmail.com>
2016-05-18 16:35:41 +0530

    restaurants page tey tabs

--- www/components/Restaurants/restaurants.js
+++ www/components/Restaurants/restaurants.js
@@ -115,6 +115,21 @@ angular.module('LoyalBonus', '')
 
         $scope.testing = 'in RestaurantController...';
 
+         $scope.goForward = function () {
+        var selected = $ionicTabsDelegate.selectedIndex();
+        console.log(selected);
+        if (selected != -1) {
+            $ionicTabsDelegate.select(selected + 1);
+        }
+    }
+
+    $scope.goBack = function () {
+        var selected = $ionicTabsDelegate.selectedIndex();
+        if (selected != -1 && selected != 0) {
+            $ionicTabsDelegate.select(selected - 1);
+        }
+    }
+
 
         $scope.print_data = [];
         $scope.data = {};
