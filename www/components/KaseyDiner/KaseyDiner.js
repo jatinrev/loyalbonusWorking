52c492939c409cb171f0311788e3353ccdde8dbf
Revinfotech <lalitdogra04@gmail.com>
2016-05-18 12:38:18 +0530

    KaseyDiner Header text

--- www/components/KaseyDiner/KaseyDiner.js
+++ www/components/KaseyDiner/KaseyDiner.js
@@ -217,6 +217,7 @@ angular.module('LoyalBonus')
                 
                 console.log(res);
                 $scope.datadeal = res.data.Data[0];
+                saveData.set('kaseyDinnerBusinessName', $scope.datadeal.Name);
                 //console.log($scope.datadeal);
                 return $scope.datadeal;
             }).then(function (res) {
