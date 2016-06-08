7543a5bd5cb7e82b6e509ffc14bd2a3aadc8d89f
jatinrev <jatin@revinfotech.com>
2016-06-08 11:48:37 +0530

    loading error in Kasey dinner

--- www/components/Product/product.js
+++ www/components/Product/product.js
@@ -3,7 +3,7 @@ angular.module('LoyalBonus')
 
         function printProduct(BusinessId) {
             console.log($rootScope.userDetails.userId);
-            loading.start();
+            // loading.start();
             return ajaxCall
                 .post('webapi/ProductList/GetProductList',
                 {
@@ -16,7 +16,7 @@ angular.module('LoyalBonus')
                 .then(function (responseResult) {
                     console.log(responseResult);
                     //return res.data.Data;
-                    loading.stop();
+                    // loading.stop();
                     return responseResult.data.Data;
                 });
         }
