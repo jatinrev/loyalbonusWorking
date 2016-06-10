01e1d2e4453b06e5248d9374bc45a7c4c4830de9
Revinfotech <lalitdogra04@gmail.com>
2016-06-10 14:43:18 +0530

    productdetail par slider done

--- www/components/Product/productDetail.js
+++ www/components/Product/productDetail.js
@@ -1,9 +1,9 @@
 angular.module('LoyalBonus')
-    .factory('productDetailFactory', function (ajaxCall,$rootScope,loading, $state) {
-        function printProductDetail(BusinessId,Productid) {
+    .factory('productDetailFactory', function (ajaxCall, $rootScope, loading, $state) {
+        function printProductDetail(BusinessId, Productid) {
             //console.log(BusinessId);
             return ajaxCall
-                .get('webapi/businessproduct/StoreProductDetails?userId=' +$rootScope.userDetails.userId +'&businessid='+$state.params.BusinessId+'&Productid=' +$state.params.Productid, {})
+                .get('webapi/businessproduct/StoreProductDetails?userId=' + $rootScope.userDetails.userId + '&businessid=' + $state.params.BusinessId + '&Productid=' + $state.params.Productid, {})
                 .then(function (responseResult) {
                     return responseResult.data.Data;
                 });
@@ -13,23 +13,15 @@ angular.module('LoyalBonus')
         };
     })
 
-    .controller('CartController', function ($scope, refreshTest, $state, ajaxCall,active_controller, $ionicPlatform,productDetailFactory) {
-        $scope.slides = [
+    .controller('CartController', function ($scope, refreshTest, $state, ajaxCall, active_controller, $ionicPlatform, productDetailFactory) {
+        /*$scope.slides = [
             { image: 'img/img00.jpg', description: 'Image 00' },
             { image: 'img/img01.jpg', description: 'Image 01' },
             { image: 'img/img02.jpg', description: 'Image 02' },
             { image: 'img/img03.jpg', description: 'Image 03' },
             { image: 'img/img04.jpg', description: 'Image 04' }
-        ];
-        $scope.hoverIn = function () {
-
-
-        };
-
-        $scope.hoverOut = function () {
-
-
-        };
+        ];*/
+       
 
 
         $scope.direction = 'left';
@@ -44,15 +36,7 @@ angular.module('LoyalBonus')
             return $scope.currentIndex === index;
         };
 
-        $scope.prevSlide = function () {
-            $scope.direction = 'left';
-            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-        };
-
-        $scope.nextSlide = function () {
-            $scope.direction = 'right';
-            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-        };
+        
 
 
 
@@ -71,9 +55,25 @@ angular.module('LoyalBonus')
                 .printProductDetail($state.params.BusinessId, $state.params.Productid)
                 .then(function (result) {
                     console.log(result);
+                    var heading_data_temp = [];
                     $scope.datadeal = result;
-                    // console.log($scope.datadeal);
+                    heading_data_temp = $scope.datadeal.ProductImages;
+                    $scope.heading_image = heading_data_temp;
+                    console.log($scope.heading_image);
                 });
+            $scope.prevSlide = function () {
+            $scope.direction = 'left';
+            $scope.currentIndex = ($scope.currentIndex < $scope.heading_image.length - 1) ? ++$scope.currentIndex : 0;
+            //alert('Right click');
+            console.log($scope.currentIndex);
+        };
+
+        $scope.nextSlide = function () {
+            $scope.direction = 'right';
+            $scope.currentIndex = ($scope.currentIndex > 0) ? -- $scope.currentIndex : $scope.heading_image.length - 1;
+            //alert('left click');
+            console.log($scope.currentIndex);
+        };
         }
         $scope.invitelistdetail();
 
